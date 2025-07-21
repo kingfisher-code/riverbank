359f9e8ad419e9374723a20a8d0816f3b3e46231
kingfisher-code <johnnwobodo.me@gmail.com>
2025-07-21 10:38:04 +0100

    second

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="w-full items-center h-screen bg-[url('/favy.jpg')] bg-center bg-cover md:bg-cover ">
       <div
-        className=" w-full h-screen bg-black/90 backdrop-blur-[2px]  justify-items-center content-center px-3
+        className=" w-full h-screen bg-black/84 backdrop-blur-[0px]  justify-items-center content-center px-3
       "
       >
         <div className="items-center ">
-          <h3 className="text-white justify-self-center text-lg font-mono ">
+          <h3 className="text-white justify-self-center text-lg font-mono text-[15px] md:text-[19px] ">
             RIVERBANK CHURCH
           </h3>
-          <h4 className="text-white/50 font-mono text-lg justify-self-center">
+          <h4 className="text-white/50 font-mono text-lg justify-self-center text-[13px] md:text-[16px]">
             Riverbank Church Media Unit...
           </h4>
         </div>
@@ -20,13 +20,13 @@ export default function Home() {
             <label className="text-white/70 font-mono justify-self-center">
               Guest Access.
             </label>
-            <span className="text-sm font-lato font-mono text-white/50">
+            <span className="text-[12px] md:text-sm font-lato font-mono text-white/50">
               Enter your email to view this collection.
             </span>
           </div>
 
           <input
-            className="bg-white text-gray-900 py-3 pl-2 font-mono "
+            className="bg-white text-gray-900 py-3 pl-2 font-mono border-2 border-blue-600  md:py-5"
             type="email"
             placeholder="Email Address"
             required
